4f9d84b63426fd1e4c501d95834950287d9ce132
RobinBilly <rbilly.mail@gmail.com>
2014-04-10 14:01:12 +0200

    Changement de lanceur GUI

--- src/views/Lanceur.java
+++ src/views/Lanceur.java
@@ -1,23 +1,20 @@
+package views;
 import java.util.ArrayList;
 
 import models.*;
 import controllers.*;
 import views.*;
 
-public class Test {
+public class Lanceur {
 
 	/**
+	 * Lancement de l'interface graphique utilisateur
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		
 		Fenetre fenetre = new Fenetre();
 		fenetre.setActivePanel(new Connexion(fenetre).launchPanel());
-		
-		/*try{
-			Thread.sleep(3000);
-		}catch(InterruptedException e){
-			System.out.println(e.getMessage());
-		}*/
 	}
 }
